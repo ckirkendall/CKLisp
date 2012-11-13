7077b36c64c80c59c58b1e730164d9cedf061a09
ckirkendall <ckirkendall@gmail.com>
2012-11-12 20:58:49 -0500

    rename lisp files to .lisp instead of .ck

--- src/main/scala/org/cklisp/CkLisp.scala
+++ src/main/scala/org/cklisp/CkLisp.scala
@@ -23,15 +23,15 @@ object CkLisp extends App {
     val reader = new Reader()
   
     val env = new ChildEnv(new NilEnv)
-    val coreLib=loadFile("org/cklisp/core.ck")   
+    val coreLib=loadFile("org/cklisp/core.lisp")   
     
     val coreExp =reader.parseAll(reader.namespace, coreLib)
     coreExp.get.map(exp => Handler.handle(exp,env))
     println(coreExp)
     args.toList match {
-      case Nil => handleFile(reader,env, "org/cklisp/repl.ck")
+      case Nil => handleFile(reader,env, "org/cklisp/repl.lisp")
       case "-f"::file::rest => println("Final Result:"+handleFile(reader,env, file))
-      case "--test"::rest => println("Final Result:"+handleFile(reader,env, "org/cklisp/test.ck"))
+      case "--test"::rest => println("Final Result:"+handleFile(reader,env, "org/cklisp/test.lisp"))
       case _ => {
           println("USAGE:")
     	  println("no args => REPL")
