c0a4c2e40bad275407782f7695ba0309b7fda8fd
ckirkendall <ckirkendall@gmail.com>
2012-11-14 16:08:11 -0500

    removed BOOL as it was not needed

--- src/main/scala/org/cklisp/Exp.scala
+++ src/main/scala/org/cklisp/Exp.scala
@@ -6,7 +6,6 @@ case class STRING(value: String) extends Exp
 case class INUM(value : Int) extends Exp
 case class FNUM(value : Double) extends Exp
 case class SYMBOL(value: Symbol) extends Exp
-case class BOOL(value: Boolean) extends Exp
 case class LIST(value: List[Exp]) extends Exp
 case class QUOTE(value: Exp) extends Exp
 case class UNQUOTE(value: Exp) extends Exp
